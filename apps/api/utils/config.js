21936131e6e7ec23bccf81d07e95177b2b0c0371
gsimpson <gs@bn.co>
2014-10-27 22:20:31 -0400

    Removing access keys. Amazon checks GitHub for their access keys?

--- apps/api/utils/config.js
+++ apps/api/utils/config.js
@@ -14,8 +14,8 @@ config = {
   PORT: env.PORT || 3001,
   QUEUE_URL: env.QUEUE_URL || 'https://sqs.us-west-2.amazonaws.com/036845378506/Mobile_Endpoint_Publication_Filtered',
   NOTIFICATION_URL: env.NOTIFICATION_URL || 'https://sqs.us-west-2.amazonaws.com/036845378506/Mobile_Endpoint_Publication_Filtered',
-  AWS_ACCESSKEYID: 'AKIAIIP6TNYSLPJ5I4UA',
-  AWS_SECRETACCESSKEY: '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',
+  AWS_ACCESSKEYID: '',
+  AWS_SECRETACCESSKEY: '',
   SNS_ARN: env.SNS_ARN || 'arn:aws:sns:us-west-2:036845378506:System_Broker_Endpoint_Notification'
 };
 
