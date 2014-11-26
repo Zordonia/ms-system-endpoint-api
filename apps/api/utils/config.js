dd762287281ce72ef4bd6769bdb86596fa9da5b2
gsimpson <gs@bn.co>
2014-11-26 00:10:09 -0500

    Adding SQS for new System endpoint.

--- apps/api/utils/config.js
+++ apps/api/utils/config.js
@@ -8,15 +8,17 @@ env = process.env;
  * Config object for the entire application.
  */
 config = {
-  MODE: env.MODE || 'development',
+  MODE: env.MODE || 'prod',
   TOKEN: env.TOKEN || 'token',
   ELASTIC_SEARCH_APIS: env.ELASTIC_SEARCH_APIS || [ 'http://localhost:9200' ],
   PORT: env.PORT || 3001,
-  QUEUE_URL: env.QUEUE_URL || 'https://sqs.us-west-2.amazonaws.com/036845378506/Mobile_Endpoint_Publication_Filtered',
-  NOTIFICATION_URL: env.NOTIFICATION_URL || 'https://sqs.us-west-2.amazonaws.com/036845378506/Mobile_Endpoint_Publication_Filtered',
-  AWS_ACCESSKEYID: '',
-  AWS_SECRETACCESSKEY: '',
-  SNS_ARN: env.SNS_ARN || 'arn:aws:sns:us-west-2:036845378506:System_Broker_Endpoint_Notification'
+  QUEUE_URL: env.QUEUE_URL || 'https://sqs.us-west-2.amazonaws.com/036845378506/Mobile_Endpoint_Publication_Filtered_EP1',
+  NOTIFICATION_URL: env.NOTIFICATION_URL || 'https://sqs.us-west-2.amazonaws.com/036845378506/Mobile_Endpoint_Publication_Filtered_EP1',
+  AWS_ACCESSKEYID: env.AWS_ACCESSKEYID || '',
+  AWS_SECRETACCESSKEY: env.AWS_SECRETACCESSKEY || '',
+  SNS_ARN: env.SNS_ARN || 'arn:aws:sns:us-west-2:036845378506:System_Broker_Endpoint_Notification',
+  POLL_DELAY: 30000,
+  ID: 'Test_System_Endpoint_1'
 };
 
 // Freeze the object that it cannot be modified during runtime.
